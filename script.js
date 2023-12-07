fc93e650b058244159af8f7f8b16767428f5f49a
Lenilson Junior <99227401+LenilsonJr27@users.noreply.github.com>
2023-12-06 22:34:28 -0300

    Update script.js [ Alteração da data da contagem // Changing the count date ]

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYear = "1 jan 2023";
+const newYear = "1 jan 2025";
 
 function countdown(){
 
